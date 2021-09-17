059b49307aac8c6af51adb5516919cc9f0bfbbd0
AdBrwebdev <broskoadam@gmail.com>
2021-09-17 16:37:10 +0200

    eshop sections created

--- src/pages/Eshop.js
+++ src/pages/Eshop.js
@@ -1,3 +1,23 @@
+import Box from '@material-ui/core/Box'
+import Typography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid'
+import Container from '@material-ui/core/Container'
+
 export default function Eshop(){
-    return(<div></div>)
+    return(<Box className="text-white container p-5 my-5">
+        <Container>
+        <Typography variant="h3" className="my-4">GPS locatory</Typography>
+        <Grid container>
+            <Grid item xs={6} sm={6} lg={6} xl={6} md={6}></Grid>
+            <Grid item xs={6} sm={6} lg={6} xl={6} md={6}></Grid>
+            <Grid item xs={6} sm={6} lg={6} xl={6} md={6}></Grid>
+        </Grid>
+        <Typography variant="h3" className="my-4">Oblečenie a doplnky </Typography>
+        <Grid container>
+            <Grid item xs={6} sm={6} lg={6} xl={6} md={6}></Grid>
+            <Grid item xs={6} sm={6} lg={6} xl={6} md={6}></Grid>
+            <Grid item xs={6} sm={6} lg={6} xl={6} md={6}></Grid>
+        </Grid>
+        </Container>
+    </Box>)
 }

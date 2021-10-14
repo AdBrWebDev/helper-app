dbe7b5c82a10f8a8cbd01dae128b91263c14f86d
AdBrwebdev <broskoadam@gmail.com>
2021-10-14 19:24:15 +0200

    Nature updated

--- src/pages/Nature.js
+++ src/pages/Nature.js
@@ -4,22 +4,74 @@ import '../App.css';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import {gsap, TimelineLite, Power3} from 'gsap';
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import Typography from '@mui/material/Typography';
+import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import '../App.css'
+
 
 export default function Nature(){
+    const [expand, setExpand] = useState('panel1');
+
     let tl = new TimelineLite();
 
     useEffect(() => {
         tl.from('img', {y: 200, opacity: 0, ease: Power3.zoomIn, delay: .5});
     })
 
+    const changePanel = (panel) => (newExpand) => {
+        setExpand(newExpand ? panel : false);
+    }
+
     return(
-        <Box>
+        <Box className="text-white text-center">
             <Box>
                 <img className='w-100' src='/images/forest.jpg' alt='forest' />
                 <Typography style={{color: "white", position: 'absolute', top: "45%"}} className="text-center" variant="h1">Pomôž nám ochrániť prírodu</Typography>
             </Box>
+            <Box className='level is-mobile mt-5'>
+                <Box className="level-item has-text-centered">
+                     <Box>
+                         <Typography className="heading">Je nás už</Typography>
+                         <Typography variant="h2" className="title text-white">50</Typography>
+                     </Box>
+                </Box>
+                <Box className="level-item has-text-centered">
+                     <Box>
+                         <Typography className="heading">Chývajúce podpisy</Typography>
+                         <Typography variant="h2" className="title text-white">799 950</Typography>
+                     </Box>
+                </Box>
+            </Box>
+            <Typography variant="h3">Prečo sa k nam pridať</Typography>
+            <Container>
+                <Card className="border border-success bg-transparent text-white my-5 p-5" id="nature-border">
+                    kaskdjfhaskjdh
+                </Card>
+            </Container>
+            <Typography variant="h3">Tak čo, zaujali sme ťa?</Typography>
+            <Typography variant="h3">Chceš pomôcť viac?</Typography>
+            <Container>
+                <Accordion expanded={expand === 'panel1'} onChange={changePanel('panel1')}>
+                    <AccordionSummary aria-controls="panel1-control" id="panel1">
+                        <Typography>Bicykel namiesto auta</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography>aklsdhfkajsh</Typography>
+                    </AccordionDetails>
+                </Accordion>
+                <Accordion expanded={expand === 'panel2'} onChange={changePanel('panel2')}>
+                    <AccordionSummary aria-controls="panel2-control" id="panel2">
+                        <Typography>Bicykel namiesto auta</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography>aklsdhfkajsh</Typography>
+                    </AccordionDetails>
+                </Accordion>
+            </Container>
             <Container>
                 <Card className="my-5 container p-5 shadow-lg" id='forest-form'>
                     <Box autoComplete="off" className="row container text-center px-5">
